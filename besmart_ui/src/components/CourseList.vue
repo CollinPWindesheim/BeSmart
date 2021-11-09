65bdb79489fb77ee6b074875565df74d18239829
collin <s1131926@student.windesheim.nl>
2021-11-09 11:38:16 +0100

    implemented course lists

--- besmart_ui/src/components/CourseList.vue
+++ besmart_ui/src/components/CourseList.vue
@@ -1,58 +1,62 @@
 <template>
-  <div id="app">
-<!--    <h1>Category = {{ cat }}</h1>-->
-    <div v-for="item in Courses" :key="item.id">
-      <v-card
-        class="mx-auto courseCard"
-        :color="color"
-        dark
-        max-width="400"
-        v-if="item.category === cat"
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="item in filterProductsByCategory"
         :key="item.id"
+        cols="12"
+        md="4"
       >
-        <v-card-title>
-          <span class="text-h6 font-weight-light">{{item.title}}</span>
-        </v-card-title>
+        <v-card
+          class="mx-auto courseCard"
+          :color="color"
+          dark
+          max-width="400"
+        >
+          <v-card-title>
+            <span class="text-h6 font-weight-light">{{item.title}}</span>
+          </v-card-title>
 
-        <v-card-text class="text-h5 font-weight-bold">
-          {{item.description}}
-        </v-card-text>
+          <v-card-text class="text-h5 font-weight-bold">
+            {{item.description}}
+          </v-card-text>
 
-        <v-card-actions>
-          <v-list-item class="grow">
-            <v-btn
-              outlined
-              rounded
-              text
-              style="width: 60%; background-color: white; color: black;"
-            >
-              Start course
-            </v-btn>
-            <v-row
-              align="center"
-              justify="end"
-            >
-              <v-btn fab small color="white">
-                <v-icon style="color: red">
-                  mdi-heart
-                </v-icon>
+          <v-card-actions>
+            <v-list-item class="grow">
+              <v-btn
+                outlined
+                rounded
+                text
+                style="width: 50%; background-color: white; color: black;"
+              >
+                Start course
               </v-btn>
-              <span class="mr-1"> </span>
-              <span class="subheading mr-2">256</span>
-              <span class="mr-1"> </span>
-              <v-btn fab small color="white">
-                <v-icon style="color: black">
-                  mdi-comment-quote
-                </v-icon>
-              </v-btn>
-              <span class="mr-1"> </span>
-              <span class="subheading">45</span>
-            </v-row>
-          </v-list-item>
-        </v-card-actions>
-      </v-card>
-    </div>
-  </div>
+<!--              <v-row-->
+<!--                align="center"-->
+<!--                justify="end"-->
+<!--              >-->
+<!--                <v-btn fab small color="white">-->
+<!--                  <v-icon style="color: red">-->
+<!--                    mdi-heart-->
+<!--                  </v-icon>-->
+<!--                </v-btn>-->
+<!--                <span class="mr-1"> </span>-->
+<!--                <span class="subheading mr-2">256</span>-->
+<!--                <span class="mr-1"> </span>-->
+<!--                <v-btn fab small color="white">-->
+<!--                  <v-icon style="color: black">-->
+<!--                    mdi-comment-quote-->
+<!--                  </v-icon>-->
+<!--                </v-btn>-->
+<!--                <span class="mr-1"> </span>-->
+<!--                <span class="subheading">45</span>-->
+<!--              </v-row>-->
+            </v-list-item>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -67,8 +71,14 @@ export default {
       Courses: [
         {id: 1, title: "What is money?", description: "This course is about money and the history of money", category: "money"},
         {id: 2, title: "Investing 101", description: "This course is about investing", category: "investing"},
-        {id: 3, title: "What is my money worth?", description: "This course is about money and what you can do with it", category: "money"}
+        {id: 3, title: "What is my money worth?", description: "This course is about money and what you can do with it", category: "money"},
+        {id: 4, title: "What is my money worth?", description: "This course is about money and what you can do with it", category: "money"}
       ],
+    }
+  },
+  computed: {
+    filterProductsByCategory: function(){
+      return this.Courses.filter(course => !course.category.indexOf(this.cat))
     }
   }
 };
